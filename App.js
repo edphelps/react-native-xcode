2ecac66046fd2250fd2ccc9800239e73b008b689
Ed Phelps <edphelps1@gmail.com>
2018-12-05 12:52:19 -0700

    about to break out Books component

--- App.js
+++ App.js
@@ -16,17 +16,93 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+/* *******************************************************
+*  App
+********************************************************** */
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
 
-      <View style={{flex: 1, flexDirection: 'column'}}>
+  state = {
+    searchCriteria: {
+      text: '',
+      authorOrTitle: 'author', // "title"
+    },
+    // ----------------
+    // DON'T DELETE!!!!
+    // ----------------
+    // The following is loaded in compomnentDidMount
+    // books: [ {
+    //     "author": "Glenn Block, et al.",
+    //     "description": "Design and build Web APIs for a broad ....",
+    //     "id": 8,
+    //     "inCart": true,
+    //     "pages": 538,
+    //     "price": 5,
+    //     "published": "2014-04-07T00:00:00.000Z",
+    //     "publisher": "O'Reilly Media",
+    //     "subtitle": "Harnessing the Power of the Web",
+    //     "title": "Designing Evolvable Web APIs with ASP.NET",
+    //     "website": "http://chimera.labs.oreilly.com/books/1234000001708/index.html"
+    //   },
+    //   {...} ]
+  }
+
+  /* **********************************
+  *  loadBooks()
+  *  Load books from the api and setState()
+  ************************************* */
+  // async loadBooks() {
+  loadBooks = async () => {
+    console.log('App:loadBooks()');
+    const response = await fetch('http://localhost:8082/api/books');
+    const json = await response.json();
+    console.log("****** books loaded", json)
+    this.setState({
+      books: json,
+    });
+  }
+
+  /* **********************************
+  *  componentDidMount()
+  *  load the books and get rendering
+  ************************************* */
+  async componentDidMount() {
+    console.log('App:componentDidMount()');
+    this.loadBooks();
+  }
+
+  /* **********************************
+  *  addToCart()
+  *  Called when user clicks the add to cart button on a book
+  *  id -- book id
+  ************************************* */
+  addToCart = async (id) => {
+    console.log('App:addToCart()');
+    const response = await fetch(`http://localhost:8082/api/books/cart/add/${id}`, {
+      method: 'PATCH',
+      body: '',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    const json = await response.json();
+    console.log('resulting json: ' + json);
+    this.loadBooks();
+  }
+
+
+  /* **********************************
+  *  render
+  ************************************* */
+  render() {
+    const { books, searchCriteria } = this.state;
+    return (
+      <View style={styles.container}>
+
         <View style={{height: 100, backgroundColor: 'powderblue'}}>
-          <Text style={styles.heading}>Bookstore</Text>
+          <Text style={styles.heading}>Bookstore 1</Text>
         </View>
-      <View>
-        <Text>List of books</Text>
         <View style={styles.container}>
           <FlatList
             data={[
@@ -42,7 +118,6 @@ export default class App extends Component<Props> {
             renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
           />
         </View>
-      </View>
       </View>
     );
   }
@@ -61,7 +136,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    padding: 20,
   },
   welcome: {
     fontSize: 20,
