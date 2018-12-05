74e244972afb72d2bd1fb05481e57bcc8020780d
Ed Phelps <edphelps1@gmail.com>
2018-12-05 13:29:17 -0700

    listing books w/ dummy buy button

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,6 +15,100 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+/* ********************************************
+*  BookList
+   Displays list of books that match the search criteria and aren't in the cart
+   books -- see App state
+   searchCriteria -- see App state
+   addToCartCB -- callback when user clicks to add book to cart, pass the book__id
+}
+*********************************************** */
+const BookList = ({ books, searchCriteria, addToCartCB }) => {
+
+  /* **********************************
+  *  render()
+  ************************************* */
+  console.log('BookList::render()');
+
+  // short circuit: still loading
+  if (!books) {
+    return (
+      <Text>Loading book list...</Text>
+    );
+  }
+  console.log("=======================")
+  console.log(books)
+  console.log("=======================")
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        renderItem={({item}) => (
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <Button title="Buy"/>
+            <Text style={styles.item}>{item.title}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+
+  // let filteredBooks = [...books];
+  //
+  // // Apply search criteria
+  // if (searchCriteria) {
+  //   filteredBooks = filteredBooks.filter((book) => {
+  //     switch (searchCriteria.authorOrTitle) {
+  //       case 'author':
+  //         return book.author.toLowerCase().startsWith(searchCriteria.text.toLowerCase());
+  //       case 'title':
+  //         return book.title.toLowerCase().startsWith(searchCriteria.text.toLowerCase());
+  //       default:
+  //         console.log('ERROR: bad searchCriteria.authorOrTitle: ', searchCriteria.authorOrTitle);
+  //         return false;
+  //     }
+  //   });
+  // };
+  //
+  // // short circuit: no books match search
+  // if (!filteredBooks.length) {
+  //   return (
+  //     <div className="container">
+  //     <h3>No books match the search</h3>
+  //     </div>
+  //   );
+  // }
+  //
+  // // filter out books already in the cart
+  // filteredBooks = filteredBooks.filter(book => !book.inCart);
+  //
+  // // short circuit: no books to display
+  // if (!filteredBooks.length) {
+  //   return (
+  //     <div className="container">
+  //       <h3>You bought everything!</h3>
+  //     </div>
+  //   );
+  // }
+  //
+  // // sort by title
+  // filteredBooks.sort((a, b) => {
+  //   if (a.title < b.title) return -1;
+  //   if (a.title > b.title) return 1;
+  //   return 0;
+  // });
+  //
+  // // render
+  // return (
+  //   <div className="container">
+  //     <h3 className="text-center">Select a title to purchase</h3>
+  //     <div className="list-group">
+  //       { filteredBooks.map(book => <BookRowContainer key={book.id} book={book} addToCartCB={addToCartCB} />) }
+  //     </div>
+  //   </div>
+  // );
+};
 
 /* *******************************************************
 *  App
@@ -91,7 +185,6 @@ export default class App extends Component<Props> {
     this.loadBooks();
   }
 
-
   /* **********************************
   *  render
   ************************************* */
@@ -103,21 +196,8 @@ export default class App extends Component<Props> {
         <View style={{height: 100, backgroundColor: 'powderblue'}}>
           <Text style={styles.heading}>Bookstore 1</Text>
         </View>
-        <View style={styles.container}>
-          <FlatList
-            data={[
-              {key: 'Devin'},
-              {key: 'Jackson'},
-              {key: 'James'},
-              {key: 'Joel'},
-              {key: 'John'},
-              {key: 'Jillian'},
-              {key: 'Jimmy'},
-              {key: 'Julie'},
-            ]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-          />
-        </View>
+
+        <BookList books={books} searchCriteria={searchCriteria} addToCartCB={this.addToCart} />
       </View>
     );
   }
